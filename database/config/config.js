26aae92d856d66a292c82976e8f3fb1b7c943da4
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-05-25 06:01:09 -0600

    - Update databse config to work properly - Add sequelize test connection to index.js

--- database/config/config.js
+++ database/config/config.js
@@ -3,14 +3,17 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    use_env_variable: 'DEV_DATABASE_URL'
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    use_env_variable: 'TEST_DATABASE_URL'
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL'
   }
 };
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import { port } from './config/environment';
 import app from './app';
+import { sequelize } from './database/models';
 
 const start = () => {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Database successfully connected');
+    })
+    .catch(err => {
+      console.log(`Database connection error: ${err}`);
+    });
+
   try {
     app.listen(port);
     console.log(`🚀  GraphQL server running at port: ${port}`);
