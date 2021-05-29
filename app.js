34ea41ed98b17faa4455d34f99736389a6385242
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-05-28 19:31:49 -0600

    - Add create react app template - Set up package.json to get ready for deployment to heroku - Set up launch.json to allow for debugging backend and front end

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
 import graphqlServer from './graphql';
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 graphqlServer.applyMiddleware({
   app
+});
+
+/* send requests to the React app */
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 export default app;
