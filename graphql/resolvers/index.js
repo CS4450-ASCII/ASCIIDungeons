b818ad6d15b5f2eee5def44c6b0ab856509b6a1e
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-06-19 22:20:46 -0600

    - Update resolver index.js so it auto imports the queries and mutations from the other folders

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,46 @@
-import { userMutations, userQueries } from './user';
+import fs from 'fs';
+import path from 'path';
+
+const basename = path.basename(__filename);
+
+const [allQueries, allMutations] = fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return file !== basename;
+  })
+  .reduce(
+    ([queries, mutations], file) => {
+      const filepath = path.join(__dirname, file);
+      let importedQueries, importedMutations;
+
+      try {
+        importedQueries = require(path.join(filepath, 'queries.js')).default;
+        importedMutations = require(path.join(
+          filepath,
+          'mutations.js'
+        )).default;
+      } catch {}
+
+      return [
+        {
+          ...queries,
+          ...importedQueries
+        },
+        {
+          ...mutations,
+          ...importedMutations
+        }
+      ];
+    },
+    [{}, {}]
+  );
 
 const resolvers = {
   Query: {
-    ...userQueries
+    ...allQueries
   },
   Mutation: {
-    ...userMutations
+    ...allMutations
   }
 };
 
