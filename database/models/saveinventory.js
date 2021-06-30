4df519849b7276fdace88e08b614ade8b195a8e5
Daniel M Gorney <daniel.m.gorney@gmail.com>
2021-06-30 15:35:08 -0600

    Updated SaveInventory Model Associations

--- database/models/saveinventory.js
+++ database/models/saveinventory.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.UserSaves);
+      this.belongsTo(models.UserSave);
       this.hasOne(models.Item);
     }
   };
