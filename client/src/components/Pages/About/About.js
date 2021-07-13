0c91f1c3841d9603d6bdb5bcc05b555d502600ed
hansonsa12 <samuelhanson@mail.weber.edu>
2021-07-12 18:10:11 -0600

    About Page Fixes #12

--- client/src/components/Pages/About/About.js
+++ client/src/components/Pages/About/About.js
@@ -1,15 +1,43 @@
-import { makeStyles } from '@material-ui/core';
+import {Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles({
-  root: {}
+  root: {
+    '& .MuiGrid-item': {
+      height: 75
+    }
+  }
 });
 
 function About(props) {
   const classes = useStyles();
   const {} = props;
 
-  return <div className={classes.root}></div>;
+  return (
+  <div className={classes.root}>
+  {(<Grid container direction = 'column' alignItems='center'>
+      <Grid item>
+        <Typography variant = 'h1'>Contributors:</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant = 'h2'>ButteredBytes</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant = 'h2'>ihavespoken</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant = 'h2'>Scruffy mmhmm</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant = 'h2'>Topped</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant = 'h2'>Free Labor</Typography>
+      </Grid>
+  </Grid>
+  )}
+  </div>
+  );
 }
 
 export default About;
