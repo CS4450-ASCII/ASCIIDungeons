f41b0e382b35f191baa7e078972b779860058bc0
hansonsa12 <samuelhanson@mail.weber.edu>
2021-07-19 18:51:59 -0600

    Finalized Models/Typedefs/Associations: Game Fixes #42

---
+++ database/models/gameentity.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class GameEntity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  GameEntity.init({
+    gameId: DataTypes.INTEGER,
+    entityId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'GameEntity',
+  });
+  return GameEntity;
+};
