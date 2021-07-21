e7e3664f75f23c2f3d5d7637d85cf87603eafbac
Daniel M Gorney <daniel.m.gorney@gmail.com>
2021-07-21 14:47:29 -0600

    Completed Level Model and Migration

---
+++ database/migrations/20210721200510-create-level.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Levels', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      gameId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Game',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      },
+      width: {
+        type: Sequelize.SMALLINT,
+        allowNull: false
+      },
+      height: {
+        type: Sequelize.SMALLINT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Levels');
+  }
+};
