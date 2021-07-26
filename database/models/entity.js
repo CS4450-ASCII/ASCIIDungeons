9c0f0675014554180c6624f26666cd9f42dbf6a4
Steven Yu <Stevenyu@mail.weber.edu>
2021-07-26 12:54:54 -0600

    associations

---
+++ database/models/entity.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Entity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Entity);
+      this.hasMany(models.GameEntity);
+    }
+  }
+  Entity.init(
+    {
+      baseType: DataTypes.SMALLINT,
+      gameEngineLayer: DataTypes.SMALLINT,
+      title: DataTypes.STRING(60),
+      character: DataTypes.STRING(1),
+      isPassable: DataTypes.BOOLEAN,
+      dataTemplate: DataTypes.JSONB
+    },
+    {
+      sequelize,
+      modelName: 'Entity'
+    }
+  );
+  return Entity;
+};
