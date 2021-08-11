a6423303f571965cc6911eddc313c8e2700fcc3b
Daniel M Gorney <daniel.m.gorney@gmail.com>
2021-08-11 14:24:30 -0600

    Added props-based data to Games.js

--- client/src/components/Pages/Profile/Pieces/Games.js
+++ client/src/components/Pages/Profile/Pieces/Games.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core';
+import React from 'react';
 import Game from './Game';
 
 const useStyles = makeStyles({
@@ -18,11 +18,11 @@ function Games(props){
   const classes = useStyles();
   const {} = props;
 
-  const arr = ["","","",""]
+  const games = props.games || ["","","",""];
 
   return <div className={classes.root}>
     <div className={classes.line}><h3>Games</h3></div>
-    {arr.map((arr) => <Game />)}
+    {games.map((game) => <Game />)}
   </div>;
 }
 
