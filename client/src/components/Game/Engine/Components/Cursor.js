44c51fd2e779b5e361b840c3c1f81499b9a16de3
Braxton Kelso <braxtogoo@hotmail.com>
2021-08-17 21:29:13 -0600

    Cursor has delete mode.

--- client/src/components/Game/Engine/Components/Cursor.js
+++ client/src/components/Game/Engine/Components/Cursor.js
@@ -32,9 +32,16 @@ export class Cursor extends GameObject {
     this.isTicking = true;
     this.MAP = null;
     this.cursorProps = cursorProps;
+
+    this.eraseMode = false;
   }
 
   step(deltatime, input) {
+    if (input.wasKeyPressed("e")) this.eraseMode = true;
+    if (input.wasKeyPressed("d")) this.eraseMode = false;
+
+    this.bColor = this.eraseMode ? "#FF0000" : "#FFFFFF";
+
     if (!this.MAP) {
       this.MAP = this.GE.getObjectByType(Map);
       if (!this.MAP) return;
@@ -48,16 +55,21 @@ export class Cursor extends GameObject {
 
     this.x = Math.floor(
       (input.MOUSE_POS.x / (this.GE.renderer.mainCanvas.width + 5)) *
-        this.GE.renderer.gridX,
+      this.GE.renderer.gridX,
     );
     this.y = Math.floor(
       (input.MOUSE_POS.y / (this.GE.renderer.mainCanvas.height + 5)) *
-        this.GE.renderer.gridY,
+      this.GE.renderer.gridY,
     );
 
     const validDataTemplate = objectHasValidDataTemplate(
       Cursor.gameObject.object,
     );
+
+    if ((input.MOUSE_CLICK || input.MOUSE_DOWN) && this.eraseMode) {
+      this.MAP.clearSpace(this.x, this.y);
+      return;
+    }
 
     if ((input.MOUSE_CLICK || input.MOUSE_DOWN) && !validDataTemplate) {
       this.MAP.setSpace(this.x, this.y, Cursor.gameObject.object);
